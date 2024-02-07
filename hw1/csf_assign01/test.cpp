a91ae343e8cdeb03795dba8404264d9f50e972fe
hshi33 <stonehj@outlook.com>
2024-02-06 22:45:25 -0500

    finish three helper function

--- hw1/csf_assign01/test.cpp
+++ hw1/csf_assign01/test.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <cstdint>
 #include <vector>
+#include "bigint.h"
 int main(){
     uint64_t left = 0x9f33ca9e0290d102;
     uint64_t right = 0x3efacbd8f95c7bb;
-    std::cout << std::hex << left - right << std::endl;
-    std::vector<uint64_t> temp;
-    temp.push_back(-1);
-    std::cout << temp[0];
+    BigInt first(left, false);
+    BigInt second(right, true);
+    BigInt temp = BigInt::subtract_magnitudes(first, second);
+    std::cout << temp.big_string[0];
 }
