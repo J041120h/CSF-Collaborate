a9eb8221da5b877aa7c06b15636428898c02eb98
hshi33 <stonehj@outlook.com>
2024-02-06 23:01:57 -0500

    finish helper function

--- hw2/c_drawing_funcs.c
+++ hw2/c_drawing_funcs.c
@@ -10,15 +10,29 @@
 ////////////////////////////////////////////////////////////////////////
 
 // TODO: implement helper functions
-int32_t in_bounds(struct Image *img, int32_t x, int32_t y); 
-uint32_t compute_index(struct Image *img, int32_t x, int32_t y);
-int32_t clamp(int32_t val, int32_t min, int32_t max);
+int32_t in_bounds(struct Image *img, int32_t x, int32_t y){
+  return clamp(x, 0, img->width) && (clamp(y, 0, img->height));
+}
+uint32_t compute_index(struct Image *img, int32_t x, int32_t y){
+  return x * (img->width) + y;
+}
+int32_t clamp(int32_t val, int32_t min, int32_t max){
+  return min < val && val < max;
+}
 uint8_t get_r(uint32_t color);
 uint8_t get_g(uint32_t color);
 uint8_t get_b(uint32_t color);
 uint8_t get_a(uint32_t color);
-uint8_t blend_components(uint32_t fg, uint32_t bg, uint32_t alpha);
-uint32_t blend_colors(uint32_t fg, uint32_t bg);
+uint8_t blend_components(uint32_t fg, uint32_t bg, uint32_t alpha){
+  return (alpha * fg + (255 - alpha) * bg) / 255;
+}
+uint32_t blend_colors(uint32_t fg, uint32_t bg){
+  uint32_t alpha = get_a(fg);
+  uint32_t red = blend_components(get_r(fg), get_r(bg), alpha);
+  uint32_t green = blend_components(get_g(fg), get_g(bg), alpha);
+  uint32_t blue = blend_components(get_b(fg), get_b(bg), alpha);
+  return red << 24 + green << 16 + blue << 8 + 255;
+}
 void set_pixel(struct Image *img, uint32_t index, uint32_t color);
 int64_t square(int64_t x);
 int64_t square_dist(int64_t x1, int64_t y1, int64_t x2, int64_t y2);
@@ -115,3 +129,5 @@ void draw_sprite(struct Image *img,
                  const struct Rect *sprite) {
   // TODO: implement
 }
+
+
