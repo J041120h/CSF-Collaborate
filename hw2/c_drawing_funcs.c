eedd0e04556e5533415b919ebeebf7f0a721de21
Hua Jiang <hjiang55@jh.edu>
2024-02-06 23:01:19 -0500

    helper function

--- hw2/c_drawing_funcs.c
+++ hw2/c_drawing_funcs.c
@@ -10,18 +10,35 @@
 ////////////////////////////////////////////////////////////////////////
 
 // TODO: implement helper functions
-int32_t in_bounds(struct Image *img, int32_t x, int32_t y); 
+int32_t in_bounds(struct Image *img, int32_t x, int32_t y);
 uint32_t compute_index(struct Image *img, int32_t x, int32_t y);
 int32_t clamp(int32_t val, int32_t min, int32_t max);
-uint8_t get_r(uint32_t color);
-uint8_t get_g(uint32_t color);
-uint8_t get_b(uint32_t color);
-uint8_t get_a(uint32_t color);
+uint8_t get_r(uint32_t color) {
+  return color >> 24;
+}
+uint8_t get_g(uint32_t color) {
+  color = color << 8;
+  return color >> 24;
+}
+uint8_t get_b(uint32_t color) {
+  color = color << 16;
+  return color >> 24;
+}
+uint8_t get_a(uint32_t color) {
+  color = color << 24;
+  return color >> 24;
+}
 uint8_t blend_components(uint32_t fg, uint32_t bg, uint32_t alpha);
 uint32_t blend_colors(uint32_t fg, uint32_t bg);
-void set_pixel(struct Image *img, uint32_t index, uint32_t color);
-int64_t square(int64_t x);
-int64_t square_dist(int64_t x1, int64_t y1, int64_t x2, int64_t y2);
+void set_pixel(struct Image *img, uint32_t index, uint32_t color) {
+  img->data[index] = color;
+}
+int64_t square(int64_t x) {
+  return x*x;
+}
+int64_t square_dist(int64_t x1, int64_t y1, int64_t x2, int64_t y2) {
+  return square(x1-x2) + square (y1-y2);
+}
 
 ////////////////////////////////////////////////////////////////////////
 // API functions
