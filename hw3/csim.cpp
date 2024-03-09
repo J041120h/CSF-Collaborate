8288d077b9219fa2a53543630c2c72615737af45
hshi33 <stonehj@outlook.com>
2024-03-08 22:09:34 -0500

    changes

--- hw3/csim.cpp
+++ hw3/csim.cpp
@@ -2,6 +2,7 @@
 #include <cstdint>
 #include <iostream>
 #include <cstdint>
+#include <vector>
 
 bool check_two_power(int n) {
     while(n != 1) {
@@ -61,4 +62,25 @@ std::pair<uint32_t, uint32_t> parse(Cache &cache, uint32_t address) {
     uint32_t tag = address >> (num_bit_offset + num_bit_index);
     uint32_t index = ( address >> (num_bit_offset) ) % (1 << num_bit_index);
     return std::make_pair(tag, index);
+}
+
+bool checkHit(Cache &cache, uint32_t index, uint32_t tag) {
+    std::vector<Slot> slots = cache.sets[index].slots;
+    for(std::vector<Slot>::iterator it = slots.begin(); it != slots.end(); it++) {
+        if(it->valid) {
+            if(it->tag == tag) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+uint32_t checkSlotAvailability(Set &set) {
+    for (int i = 0; i < set.maxSlots; i++) {
+        if(!(set.slots[i].valid)) {
+            return i;
+        }
+    }
+    return -1;
 }
