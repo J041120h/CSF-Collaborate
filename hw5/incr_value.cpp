c123dd6007bae3ac6e8f3664a7ae15f2b7dc02d1
hshi33 <stonehj@outlook.com>
2024-04-12 00:00:36 -0400

    finish MS1 largely

--- hw5/incr_value.cpp
+++ hw5/incr_value.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
+#include "message.h"
+#include "csapp.h"
+#include "message_serialization.h"
+#include "exceptions.h"
 
 int main(int argc, char **argv) {
   if ( argc != 6 && (argc != 7 || std::string(argv[1]) != "-t") ) {
@@ -23,4 +27,52 @@ int main(int argc, char **argv) {
   std::string key = argv[count++];
 
   // TODO: implement
+  Message beginMessage = Message(MessageType::BEGIN);
+  Message logMessage = Message(MessageType::LOGIN, {username});
+  Message pushMessage = Message(MessageType::PUSH, {"1"});
+  Message setMessage = Message(MessageType::SET, {table, key});
+  Message commitMessage = Message(MessageType::COMMIT);
+  Message byeMessage = Message(MessageType::BYE);
+  std::vector<Message> messageList = {beginMessage, logMessage, pushMessage, setMessage, commitMessage, byeMessage};
+  int fd = open_clientfd(hostname.data(), port.data());
+  if (fd < 0) {
+    std::cerr << "Error: cannot connect to server" << std::endl;
+    return -1;
+  }
+  for (std::vector<Message>::const_iterator it = messageList.begin(); it != messageList.end(); it++) {
+    if (!it->is_valid()) {
+      std::cerr << "Error: attempt to send invalid message" << std::endl;
+    }
+    std::string inputMessage = "";
+    try {
+      MessageSerialization::encode(*it, inputMessage);
+    } catch (InvalidMessage &ex) {
+      std::cerr << "Error: cannot encode message" << std::endl;
+    }
+    rio_writen(fd, inputMessage.data(), inputMessage.length());
+    rio_t rio;
+    char buf[2048];
+    rio_readinitb(&rio, fd);
+    ssize_t n = rio_readlineb(&rio, buf, sizeof(buf));
+    Message responseMessage;
+    try {
+      const std::string message(buf);
+      MessageSerialization::decode(message, responseMessage);
+    } catch (InvalidMessage &ex) {
+      std::cerr << "Error: cannot decode message" << std::endl;
+      return -1;
+    }
+    if (!responseMessage.is_valid()) {
+      std::cerr << "Error: invalid reply message" << std::endl;
+      return -1;
+    }
+    if (responseMessage.get_message_type() == MessageType::FAILED) {
+      std::cerr << "Error: " << responseMessage.get_arg(0) << std::endl;
+      return -1;
+    } else if (responseMessage.get_message_type() == MessageType::ERROR) {
+      std::cerr << "Error:" << responseMessage.get_arg(0) << std::endl;
+      return -1;
+    }
+  }
+  return 0;
 }
